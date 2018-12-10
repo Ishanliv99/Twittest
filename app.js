3d1604bc450d447fe0e58bc6203254e5742f1d12
ishanliv99 <ishanliv99@gmail.com>
2018-12-10 18:58:27 +0545

    Add react

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
+const path = require('path');
 const logger = require('morgan');
 const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
+
+app.use(express.static(path.join(__dirname, '../frontend/build')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '../frontend/build/index.html'));
+});
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
